f8e07fbe7911b4edc20344f0dae3182b5bd174a6
Ondřej Trnovec <ondrej.trnovec9@gmail.com>
2024-10-12 17:04:01 +0200

    should be finished

--- extract_project_descriptions_and_public_interests.py
+++ extract_project_descriptions_and_public_interests.py
@@ -1,5 +1,5 @@
-""" This file is for getting the project descritptions and 
-public interest from xml and save them to a csv file. """
+""" This file is for getting the project descriptions and 
+public interests from xml and save them to a csv file. """
 import time
 import re
 import requests
@@ -22,28 +22,32 @@ def extract_project_description_and_public_interest(url_df):
         list_of_project_ids.append(project_id)
 
         #get the url content
-        url = requests.get(url)
+        url = requests.get(url, timeout=5)
         soup = BeautifulSoup(url.content, 'html.parser')
 
         #extract the project description from xml and save to the list
         project_description = soup.find("div", class_="project-description").text
-        project_description = re.sub('\t', '', project_description)
-        #project_description = project_description.find("p").text
+        #remove the titles "Hlasovací video" and "Realizační video" from the description
+        project_description = re.sub('Hlasovací video', '', project_description)
+        project_description = re.sub('Realizační video', '', project_description)
+        #remove leading and trailing whitespaces
+        project_description = project_description.strip()
         list_of_project_descriptions.append(project_description)
 
         #extract the public interest from xml and save to the list
         project_public_interest = soup.find("div", class_="col-xs-12 col-sm-offset-2 col-sm-8")
         project_public_interest = project_public_interest.find("p")
         project_public_interest = project_public_interest.find("i").text
-
-        project_public_interest = re.sub('\t', '', project_public_interest)
+        #remove leading and trailing whitespaces
+        project_public_interest = project_public_interest.strip()
         list_of_public_interest.append(project_public_interest)
 
         #delay in seconds so we dont get blocked by the site
-        DELAY = 3
+        DELAY = 1
         time.sleep(DELAY)
 
     return list_of_project_ids, list_of_project_descriptions, list_of_public_interest
+
 
 def save_to_csv(list_of_project_ids, list_of_project_descriptions, list_of_public_interest):
     """Function to save the lists to a csv file."""
@@ -53,14 +57,19 @@ def save_to_csv(list_of_project_ids, list_of_project_descriptions, list_of_publi
         "project_description": list_of_project_descriptions,
         "public_interest": list_of_public_interest
         })
-    print(output_df)
+    
     #save the dataframe to a csv file
-    output_df.to_csv('descriptions_and_interests.csv', sep='\t', encoding='utf-8')
+    output_df.to_csv('descriptions_and_interests.csv', sep=',', encoding='utf-8')
 
 
 #convert csv to df
 df = pd.read_csv("data\ProjektyPARO_5358953113614861487.csv")
+
+#extract the url list
 url_list = df["properties.detail"]
+
+#call the extract function
 lists = extract_project_description_and_public_interest(url_list)
 
+#call the save function
 save_to_csv(lists[0], lists[1], lists[2])
