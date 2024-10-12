b9c9186d514f9bfb02134a33abb022b6326f281f
Ondřej Trnovec <ondrej.trnovec9@gmail.com>
2024-10-12 22:32:17 +0200

    progress

--- extracting_assesment_of_feasibility.py
+++ extracting_assesment_of_feasibility.py
@@ -7,19 +7,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def extract_project_description_and_public_interest(url_df):
+def extract_assesment_of_feasibility(url_df):
     """Function to extract project description and public interest from the xml."""
 
     #initialize lists
-    list_of_project_ids = []
-    list_of_project_descriptions = []
-    list_of_public_interest = []
+    list_of_lists = []
 
     #loop through the urls in the csv
     for url in url_df:
         #get project_id from url and save to the list
         project_id = url.split("id=",1)[1]
-        list_of_project_ids.append(project_id)
+        list = [project_id]
+        
 
         #get the url content
         url = requests.get(url, timeout=5)
@@ -29,56 +28,46 @@ def extract_project_description_and_public_interest(url_df):
         if soup.find("div", {"id": "project-block-reaction"}):
             assesment_of_feasibility = soup.find("div", {"id": "project-block-reaction"})
             assesment_of_feasibility = assesment_of_feasibility.find("tbody")
-            assesment_of_feasibility = assesment_of_feasibility.find_all("tr")
-            print(assesment_of_feasibility[1])
+            assesment_of_feasibility = assesment_of_feasibility.find_all("td")
+            count = 0
+            for td in assesment_of_feasibility:
+                if len(td.text) > 0:
+                    list.append(td.text)
+                else:
+                    print("empty")
+                    list.append("EMPTY")
+
+                count += 1
+                if count == 3:
+                    list_of_lists.append(list)
+                    list = [project_id]
+                    count = 0
+
         else:
-        
+            list.append("missing assesment of feasibility")
+            list.append("missing assesment of feasibility")
+            list.append("missing assesment of feasibility")
+            list_of_lists.append(list)
         
             print("missing assesment of feasibility")
-        #project_description = soup.find("div", class_="project-description").text
-
-
-
-#         #remove leading and trailing whitespaces
-#         project_description = project_description.strip()
-#         list_of_project_descriptions.append(project_description)
-
-#         #extract the public interest from xml and save to the list
-#         project_public_interest = soup.find("div", class_="col-xs-12 col-sm-offset-2 col-sm-8")
-#         project_public_interest = project_public_interest.find("p")
-#         project_public_interest = project_public_interest.find("i").text
-#         #remove leading and trailing whitespaces
-#         project_public_interest = project_public_interest.strip()
-#         list_of_public_interest.append(project_public_interest)
-
-#         #delay in seconds so we dont get blocked by the site
-#         DELAY = 1
-#         time.sleep(DELAY)
-
-#     return list_of_project_ids, list_of_project_descriptions, list_of_public_interest
-
-
-# def save_to_csv(list_of_project_ids, list_of_project_descriptions, list_of_public_interest):
-#     """Function to save the lists to a csv file."""
-#     #convert lists to dataframe
-#     output_df = pd.DataFrame(
-#         {"project_id": list_of_project_ids,
-#         "project_description": list_of_project_descriptions,
-#         "public_interest": list_of_public_interest
-#         })
+            
+    return list_of_lists
     
-#     #save the dataframe to a csv file
-#     output_df.to_csv('descriptions_and_interests.csv', sep=',', encoding='utf-8')
+
+def save_to_csv(list_of_lists):
+    """Function to save the lists to a csv file."""
+    #convert lists to dataframe
+    output_df = pd.DataFrame(list_of_lists, columns = ['ID', 'Subjekt', 'Odůvodnění', 'Závěr'])
+
+    #save the dataframe to a csv file
+    output_df.to_csv('assesment_of_feasibility.csv', sep=',', encoding='utf-8')
 
 
 # #convert csv to df
 df = pd.read_csv("data\ProjektyPARO_5358953113614861487.csv")
 
-# #extract the url list
+#extract the url list
 url_list = df["properties.detail"]
 
-# #call the extract function
-lists = extract_project_description_and_public_interest(url_list)
-
-# #call the save function
-# save_to_csv(lists[0], lists[1], lists[2])
+#call the save function
+save_to_csv(extract_assesment_of_feasibility(url_list))
