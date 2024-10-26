39d62f3ded3e511e24fb288c5c6dfb15fae23e9c
Jan Malý <x526325@fi.muni.cz>
2024-10-26 18:02:51 +0200

    added stop words removal

--- basic_text_model_WIP.py
+++ basic_text_model_WIP.py
@@ -4,10 +4,27 @@ from sklearn.feature_extraction.text import CountVectorizer     # CountVectorize
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+from pathlib import Path
+
+czech_stop_list = [
+    "být", "v", "a", "sebe", "na", "ten", "s", "z", "že", "který", "o", "mít", "i", "do", "on", "k",
+    "pro", "tento", "za", "by", "moci", "svůj", "ale", "po", "rok", "jako", "však", "od", "všechen",
+    "dva", "nebo", "tak", "u", "při", "jeden", "podle", "Praha", "jen", "další", "jeho", "aby", "co",
+    "český", "jak", "veliký", "nový", "až", "už", "muset", "než", "nebýt", "člověk", "jenž", "léto",
+    "firma", "první", "náš", "také", "my", "jejich", "když", "před", "doba", "chtít", "jiný", "mezi",
+    "ještě", "já", "ani", "cena", "již", "jít", "strana", "či", "druhý", "pouze"
+]
 
 
-def prepare_data():
-    lemmatized_df = pd.read_csv("data/lemmatized_df_small.csv", sep=",",
+def remove_stop_words(input_string: str, stop_list: list = czech_stop_list) -> str:
+    words = input_string.split()
+    filtered_words = [word for word in words if word not in stop_list]
+    output_string = ' '.join(filtered_words)
+    return output_string
+
+
+def load_and_split_data() -> tuple:
+    lemmatized_df = pd.read_csv(Path(__file__).parent / "data" / "lemmatized_df_small.csv", sep=",",
                                 usecols=["project_name", "project_description", "public_interest", "status"])
 
     lemmatized_df["project_name"] = lemmatized_df["project_name"].astype("string")
@@ -17,20 +34,23 @@ def prepare_data():
     df_train, df_val = lemmatized_df[:65], lemmatized_df[65:]
 
     cols = ["project_name", "project_description", "public_interest"]
+
     X_train = df_train[cols].apply(
         lambda row: '_'.join(row.values.astype(str)), axis=1)
+    X_train = X_train.apply(remove_stop_words)
     y_train = df_train["status"]
 
     X_val = df_val[cols].apply(
         lambda row: '_'.join(row.values.astype(str)), axis=1)
+    X_val = X_val.apply(remove_stop_words)
     y_val = df_val["status"]
 
     return X_train, y_train, X_val, y_val
 
 
 if __name__ == "__main__":
-    X_train, y_train, X_val, y_val = prepare_data()
-
+    X_train, y_train, X_val, y_val = load_and_split_data()
+    print(X_train[:5])
     pipeline = Pipeline([
             ('vectorizer', CountVectorizer()),
             # ('classifier', LogisticRegression(max_iter=100, random_state=42)),
