81965d41275e2d6949af7872c5e6b3803f12f8b0
Ondřej Trnovec <83825838+otrnovec@users.noreply.github.com>
2024-10-31 20:04:45 +0100

    resolving comments, proveditelný converting to 1

--- lemmatize_text_data.py
+++ lemmatize_text_data.py
@@ -1,12 +1,12 @@
-from settings import DATA_DIR, MODEL_DIR
+from settings import DATA_DIR, MORPHODITA_MODEL_DIR
 import os
 
 import pandas as pd
 from morphodita import get_lemmas
 
 
-def get_lemmatized_df(dataframe: pd.DataFrame, tagger_file):
-    """Get lemmatized text data from the dataframe."""
+def generate_lemmatized_dataset(dataframe: pd.DataFrame, tagger_file, output_file):
+    """Generate lemmatized text data from the dataframe."""
 
     name_lemmatized = get_lemmas(tagger_file, dataframe["project_name"].tolist())
     description_lemmatized = get_lemmas(
@@ -27,17 +27,20 @@ def get_lemmatized_df(dataframe: pd.DataFrame, tagger_file):
             "feasible": 1,
             "winning": 1,
             "unfeasible": 1,
+            "proveditelný": 1,
             "without support": 0,
         },
         inplace=True,
     )
 
-    df.to_csv(DATA_DIR / "lemmatized_dataset.csv", index=False)
+    df.to_csv(DATA_DIR / output_file, index=False)
 
 
 if __name__ == "__main__":
     # Replace with your tagger file path
-    tagger_file = os.path.join(MODEL_DIR, "czech-morfflex2.0-pdtc1.0-220710.tagger")
+    tagger_file = os.path.join(
+        MORPHODITA_MODEL_DIR, "czech-morfflex2.0-pdtc1.0-220710.tagger"
+    )
     df = pd.read_csv(DATA_DIR / "paro_preprocessed.csv")
 
-    get_lemmatized_df(df, tagger_file)
+    generate_lemmatized_dataset(df, tagger_file, "lemmatized_dataset.csv")
