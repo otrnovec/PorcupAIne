3636fdaad5b92ee635dae2b7de35fbbe8d0d6fa7
Pavel Oujeský <oujeskypavel.trading@gmail.com>
2024-12-11 11:41:13 +0100

    GUI improvements based on feedback

--- porcupaine/web/main.py
+++ porcupaine/web/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles  # Import pro statické soubory
+#from porcupaine.porcupaine_score import compute_porcupaine_score
 import uvicorn
+import json  # Pro práci s JSON souborem
 
 # Initialize the FastAPI app
 app = FastAPI()
@@ -12,6 +14,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Specify the templates directory
 templates = Jinja2Templates(directory="templates")
+
+# Načti data z JSON souboru
+with open("static/data.json", encoding="utf-8") as f:
+    data = json.load(f)
+
+# Vytvoř slovníky pro vyhledávání
+categories = {item["value"]: item["label"] for item in data["categories"]}
+districts = {item["value"]: item["label"] for item in data["districts"]}
 
 # Route for displaying the form
 @app.get("/", response_class=HTMLResponse)
@@ -29,7 +39,11 @@ async def submit_form(
     category: str = Form(...),
     budget: str = Form(...)
 ):
-    # Process the form data as needed
+    # Nahrazení value za label
+    district_label = districts.get(district, "Neznámá oblast")
+    category_label = categories.get(category, "Neznámá kategorie")
+
+    # Předání popisků do šablony
     return templates.TemplateResponse(
         "result.html",
         {
@@ -37,11 +51,12 @@ async def submit_form(
             "name": name,
             "description": description,
             "public_interest": public_interest,
-            "district": district,
-            "category": category,
+            "district": district_label,
+            "category": category_label,
             "budget": budget,
         },
     )
+
 
 # Run the app with Uvicorn
 if __name__ == "__main__":
